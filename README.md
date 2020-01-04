ac2953c3afce9cc9039997fb7d2d6ef2070531c1
Byebrid <lexlmgallon@gmail.com>
2020-01-04 15:41:44 +1100

    Added date functionality to searchbar
    - Made it so user can search for a date to retrieve relevant Europe blog
       posts. User can also simply pass in a month name to get all posts
       from that month.
    - Added "no results founds" message if user search returns nothing
    - Minor addition to README
    - Other minor changes

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ For development of byebrid.com
 **Notes**
 =========
 - Need to ensure that ckeditor is not accessible to anyone but admins since it is not escaped safely and so could be abused.
+
+**To do**
+=========
+- Make it so if user searches for Europe blog post, they get some feedback of what exactly they put in up top (i.e. Search results for "Geneva"). Not sure if this will necessitate brand new template just for search queries (I assume not but not quite sure how to implement yet).
--- django_project/europe_blog/views.py
+++ django_project/europe_blog/views.py
@@ -9,6 +9,9 @@ from django.views.generic import (
     DeleteView
 )
 from django.db.models import Q # for querysets
+import datetime
+import dateutil.parser
+import calendar
 from .models import EuropePost
 
 
@@ -23,11 +26,53 @@ class EuropePostDetailView(DetailView):
 
 
 class EuropePostSearchView(ListView):
+    """A view to process the user's search query.
+    
+    Will attempt to parse dates and find relevant posts. Allows for somewhat
+    fuzzy parsing (i.e. assumes year to be 2019 since that was the year of the
+    trip) and allows user to simply put in a month (or acceptable shorthand such
+    as 'jun' for June) and get all relevant posts.
+
+    Note: If user looks for the word 'may', this will obviously get interpreted as
+    the month so tough luck if they want the actual word.
+    """
     model = EuropePost
     template_name = 'europe_blog/home.html' # same as normal list view
 
     def get_queryset(self):
         query = self.request.GET.get('q')
+
+        # Test if user passed in date. If so, test against dates of trip
+        try:
+            query_as_date = dateutil.parser.parse(query, fuzzy=True)
+            query_as_date = query_as_date.replace(year=2019) # trip was in 2019
+
+            # __init__ ensures months are converted to lower-case, which is nice
+            parserinfo = dateutil.parser.parserinfo()
+
+            # If user only passes month in, then get all posts for that month
+            if query.lower() in parserinfo._months:
+                query_month = query_as_date.month # Get the month as int, not str
+                start_of_month = datetime.date(2019, query_month, 1)
+                # monthrange returns tuple of (first day, last day) in month
+                n_days_in_month = calendar.monthrange(2019, query_month)[1]
+                end_of_month = datetime.date(2019, query_month, n_days_in_month)
+
+                # Get all posts from sometime in this month
+                result = EuropePost.objects.filter(
+                    Q(arrival_date__gte=start_of_month, arrival_date__lte=end_of_month) |
+                    Q(departure_date__gte=start_of_month, departure_date__lte=end_of_month)
+                )
+                return result
+            else:
+                result = EuropePost.objects.filter(arrival_date__lte=query_as_date, departure_date__gte=query_as_date)
+                return result
+
+        except Exception as e:
+            print(e)
+            pass
+        
+        # If query not recognised as date, compare with contents of blog posts
         return EuropePost.objects.filter(
             Q(location__icontains=query) | Q(content__icontains=query)
         )
