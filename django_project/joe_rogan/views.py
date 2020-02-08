c6c41600f442696d3192d8f325d233cad6c53ea7
Byebrid <lexlmgallon@gmail.com>
2020-02-09 09:51:30 +1100

    Add last updated to Joe Rogan app
    - Add html showing when Joe Rogan csv was last updated
    - Fix up list formatting on joe rogan app

--- django_project/joe_rogan/views.py
+++ django_project/joe_rogan/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.conf import settings
 from django.core.paginator import Paginator
 import os
+import datetime
+import pytz
 
 BASE_DIR = settings.BASE_DIR
 
@@ -11,12 +13,18 @@ def home(request):
     with open(filepath, 'r') as f:
         quotes = [line for line in f]
 
+    last_updated_time = os.path.getmtime(filepath)
+    tz = pytz.timezone('Australia/Melbourne')
+    last_updated_date = datetime.datetime.fromtimestamp(last_updated_time)
+    last_updated_display = last_updated_date.strftime('%a %-d %b, %Y')
+
     paginator = Paginator(quotes, 50)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     context = {
         'title': "Joe \"Joe Rogan\" Rogan",
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'last_updated': last_updated_display
     }
     return render(request, 'joe_rogan/home.html', context=context, )
