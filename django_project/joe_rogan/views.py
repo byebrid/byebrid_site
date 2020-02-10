d8734ff58c6c1ae10528471ff51983e1cdc868e7
Byebrid <lexlmgallon@gmail.com>
2020-02-10 13:22:58 +1100

    Improve visuals of Joe Rogan app, use json now
    - Improve visuals of Joe Rogan app. Also, decided I should make a model
      for the video 'articles' for very easy pagination. Just commit this
      because it works.
    - Redo how we handle video objects since we now load from json file, not
      csv.

--- django_project/joe_rogan/views.py
+++ django_project/joe_rogan/views.py
@@ -4,27 +4,37 @@ from django.core.paginator import Paginator
 import os
 import datetime
 import pytz
+import json
 
 BASE_DIR = settings.BASE_DIR
 
 
 def home(request):
-    filepath = os.path.join(BASE_DIR, 'joe_rogan', 'joe_rogan.csv')
+    filepath = os.path.join(BASE_DIR, 'joe_rogan', 'joe_rogan.json')
     with open(filepath, 'r') as f:
-        quotes = [line for line in f]
+        videos = json.load(f)
+
+    for title in list(videos.keys()):
+        if len(videos[title]['quotes']) == 0:
+            videos.pop(title)
+        else:
+            id = videos[title]['id']
+            url = f'https://www.youtube.com/watch?v={id}'
+            videos[title]['url'] = url
 
     last_updated_time = os.path.getmtime(filepath)
     tz = pytz.timezone('Australia/Melbourne')
-    last_updated_date = datetime.datetime.fromtimestamp(last_updated_time)
+    last_updated_date = datetime.datetime.fromtimestamp(last_updated_time, tz)
     last_updated_display = last_updated_date.strftime('%a %-d %b, %Y')
 
-    paginator = Paginator(quotes, 50)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    # paginator = Paginator(quotes, 50)
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
 
     context = {
         'title': "Joe \"Joe Rogan\" Rogan",
-        'page_obj': page_obj,
+        'videos': videos,
+        # 'page_obj': page_obj,
         'last_updated': last_updated_display
     }
     return render(request, 'joe_rogan/home.html', context=context, )
