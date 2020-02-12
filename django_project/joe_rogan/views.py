e21be58f86a572cb938dbadb5a5e9c36e10caced
Byebrid <lexlmgallon@gmail.com>
2020-02-12 19:19:40 +1100

    Bug fix where last_updated was unavailable; currently no solution so simply removed references to last updated time in views and template

--- django_project/joe_rogan/views.py
+++ django_project/joe_rogan/views.py
@@ -18,17 +18,3 @@ class JoeRoganListView(ListView):
     template_name = 'joe_rogan/home.html'
 
     paginate_by = 5
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super().get_context_data(**kwargs)
-
-        # Adding last_updated attribute to know when json was last modified
-        filepath = os.path.join(BASE_DIR, 'joe_rogan', 'joe_rogan.json')
-        last_updated_time = os.path.getmtime(filepath)
-        tz = pytz.timezone('Australia/Melbourne')
-        last_updated_date = datetime.datetime.fromtimestamp(last_updated_time, tz)
-        last_updated_display = last_updated_date.strftime('%a %-d %b, %Y')
-
-        context['last_updated'] = last_updated_display
-        return context
